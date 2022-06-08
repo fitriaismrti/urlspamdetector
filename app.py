f15042873720b3673655f7a8515bcd5e226e702e
fitriaismrti <106650282+fitriaismrti@users.noreply.github.com>
2022-06-08 19:21:54 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import validators
 model = pickle.load(open("final_model.pkl", "rb"))
 
 st.title("URL Spam Checker")
-st.write("We made this application to help you find out if a link is spam or not, you can check a link before click it to keep your device safe. Randomly clicking links or going to unknown websites on your computer, phone, or even smart TV can put you at risk of covertly downloaded software intended to damage or disable your computer or other devices. We made this application using the Naive Bayes and Simplification method. Then, we tested using a dataset collection of spam and non-spam urls that we got from kaggle. We hope that with this application people can be vigilant in accessing a link whose source is not clear.")
+st.write("We made this application to help you find out if a link is spam or not, you can check a link before click it to keep your device safe. Randomly clicking links or going to unknown websites on your computer, phone, or even smart TV can put you at risk of covertly downloaded software intended to damage or disable your computer or other devices. We made this application using the Naive Bayes method. Then, we tested using a dataset collection of spam and non-spam urls that we got from kaggle. We hope that with this application people can be vigilant in accessing a link whose source is not clear.")
 
 link = st.text_input(label = "Input URL", placeholder = "Insert your url here")
 
